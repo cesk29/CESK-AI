c08135db2f45e34fe8add6bb79b5b14fe6c25f6c
cesk29 <f.criscuolo1@ied.edu>
2023-12-05 22:26:12 +0100

    Update 05/12

--- cesk_ai/lib/src/presentation/home/widgets/sidebar.dart
+++ cesk_ai/lib/src/presentation/home/widgets/sidebar.dart
@@ -7,43 +7,349 @@ class Sidebar extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       width: 300,
-      child: Column(children: [
-        Row(
-          children: [
-            Container(),
-            Padding(
-              padding: const EdgeInsets.only(right: 8),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Image.network(
-                  'https://static-00.iconduck.com/assets.00/openai-icon-2021x2048-4rpe5x7n.png',
-                  width: 20,
+      child: Container(
+        decoration: const BoxDecoration(
+          color: Color.fromRGBO(0, 0, 0, 1),
+        ),
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Row(
+              children: [
+                Container(),
+                Padding(
+                  padding: const EdgeInsets.only(right: 8),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(100),
+                    child: Image.network(
+                      'https://static.vecteezy.com/system/resources/previews/019/493/370/original/chatgpt-logo-openai-logo-ai-chatbot-chatgpt-copy-space-text-editorial-illustration-free-vector.jpg',
+                      width: 30,
+                    ),
+                  ),
+                ),
+                const Text(
+                  'ChatGPT',
+                  style: TextStyle(
+                    fontSize: 14,
+                    fontFamily: 'Nimbus',
+                    fontWeight: FontWeight.w200,
+                    color: Color.fromRGBO(255, 255, 255, 1),
+                  ),
+                ),
+                const Spacer(),
+                const Icon(
+                  Icons.add,
+                  color: Color.fromRGBO(255, 255, 255, 1),
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: const [
+                Padding(
+                  padding: EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.only(top: 16),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Help me debug',
+                              style: TextStyle(
+                                  fontSize: 10,
+                                  fontFamily: 'Nimbus',
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromRGBO(160, 160, 160, 1)),
+                            ),
+                            Text(
+                              'Help me debug my script please',
+                              style: TextStyle(
+                                  fontSize: 13,
+                                  color: Color.fromRGBO(255, 255, 255, 1)),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 24),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Finding bugs is part of development',
+                              style: TextStyle(
+                                fontSize: 10,
+                                fontFamily: 'Nimbus',
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromRGBO(160, 160, 160, 1),
+                              ),
+                            ),
+                            Text(
+                              'Code reviews improve code quality',
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Color.fromRGBO(255, 255, 255, 1),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 24),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Programming requires creativity',
+                              style: TextStyle(
+                                fontSize: 10,
+                                fontFamily: 'Nimbus',
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromRGBO(160, 160, 160, 1),
+                              ),
+                            ),
+                            Text(
+                              'Efficient code is easy to maintain',
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Color.fromRGBO(255, 255, 255, 1),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 24),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Debugging makes you a better coder',
+                              style: TextStyle(
+                                fontSize: 10,
+                                fontFamily: 'Nimbus',
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromRGBO(160, 160, 160, 1),
+                              ),
+                            ),
+                            Text(
+                              'Practice makes perfect in programming',
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Color.fromRGBO(255, 255, 255, 1),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 72),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Coding is about problem-solving',
+                              style: TextStyle(
+                                fontSize: 10,
+                                fontFamily: 'Nimbus',
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromRGBO(160, 160, 160, 1),
+                              ),
+                            ),
+                            Text(
+                              'Keep your code clean and organized',
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Color.fromRGBO(255, 255, 255, 1),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 24),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Debugging is like detective work',
+                              style: TextStyle(
+                                fontSize: 10,
+                                fontFamily: 'Nimbus',
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromRGBO(160, 160, 160, 1),
+                              ),
+                            ),
+                            Text(
+                              'Programming opens up endless possibilities',
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Color.fromRGBO(255, 255, 255, 1),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 24),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Coding is the language of the future',
+                              style: TextStyle(
+                                fontSize: 10,
+                                fontFamily: 'Nimbus',
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromRGBO(160, 160, 160, 1),
+                              ),
+                            ),
+                            Text(
+                              'Programmers create the digital world',
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Color.fromRGBO(255, 255, 255, 1),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 72),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Debugging is a puzzle-solving skill',
+                              style: TextStyle(
+                                fontSize: 10,
+                                fontFamily: 'Nimbus',
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromRGBO(160, 160, 160, 1),
+                              ),
+                            ),
+                            Text(
+                              'Great coders write maintainable code',
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Color.fromRGBO(255, 255, 255, 1),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 32),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Debugging makes you a better coder',
+                              style: TextStyle(
+                                fontSize: 10,
+                                fontFamily: 'Nimbus',
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromRGBO(160, 160, 160, 1),
+                              ),
+                            ),
+                            Text(
+                              'Practice makes perfect in programming',
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Color.fromRGBO(255, 255, 255, 1),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 24),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Coding is about problem-solving',
+                              style: TextStyle(
+                                fontSize: 10,
+                                fontFamily: 'Nimbus',
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromRGBO(160, 160, 160, 1),
+                              ),
+                            ),
+                            Text(
+                              'Keep your code clean and organized',
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Color.fromRGBO(255, 255, 255, 1),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 24),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Debugging is like detective work',
+                              style: TextStyle(
+                                fontSize: 10,
+                                fontFamily: 'Nimbus',
+                                fontWeight: FontWeight.bold,
+                                color: Color.fromRGBO(160, 160, 160, 1),
+                              ),
+                            ),
+                            Text(
+                              'Programming opens up endless possibilities',
+                              style: TextStyle(
+                                fontSize: 13,
+                                color: Color.fromRGBO(255, 255, 255, 1),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const Row(
+            children: [
+              Padding(
+                padding: EdgeInsets.all(16),
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(right: 12),
+                      child: CircleAvatar(
+                        radius: 18.0,
+                        backgroundImage: NetworkImage(
+                            'https://cdn.dribbble.com/users/1787323/screenshots/4458591/dribbbe_bulbasaur-01.png'),
+                      ),
+                    ),
+                    Text(
+                      'Billy Bulbasaur',
+                      style: TextStyle(
+                        fontSize: 14,
+                        fontFamily: 'Nimbus',
+                        fontWeight: FontWeight.w600,
+                        color: Color.fromRGBO(255, 255, 255, 1),
+                      ),
+                    ),
+                  ],
                 ),
               ),
-            ),
-            const Text('ChatGPT'),
-            const Spacer(),
-            const Icon(Icons.add),
-          ],
-        ),
-        Expanded(
-          child: ListView(),
-        ),
-        Row(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 8),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(8),
-                child: Image.network(
-                  'https://t0.gstatic.com/licensed-image?q=tbn:ANd9GcSafEPPinDM7CIUSO4NnEXQsDUopcafuTQ_7YJPcmrYf6ELHpbVzVp8KVkzRE1zh1AO',
-                  width: 30,
-                ),
-              ),
-            ),
-          ],
-        ),
-      ]),
+            ],
+          ),
+        ]),
+      ),
     );
   }
 }
