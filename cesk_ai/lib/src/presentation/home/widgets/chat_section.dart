2dc28d11ee21c0f0a6e932783603901182dd534e
cesk29 <f.criscuolo1@ied.edu>
2024-01-10 11:55:21 +0100

    update

--- cesk_ai/lib/src/presentation/home/widgets/chat_section.dart
+++ cesk_ai/lib/src/presentation/home/widgets/chat_section.dart
@@ -79,111 +79,108 @@ class ChatSection extends StatelessWidget {
       }
     ];
     return Expanded(
-      child: Align(
-        alignment: Alignment.center,
-        child: Stack(
-          children: [
-            Container(
-                decoration: const BoxDecoration(
-                    gradient: LinearGradient(
-              begin: Alignment.centerLeft,
-              end: Alignment.bottomRight,
-              stops: [
-                0.1,
-                0.4,
-                0.8,
-                0.9,
-              ],
-              colors: [
-                Color.fromARGB(255, 0, 0, 0),
-                Color.fromARGB(255, 0, 43, 43),
-                Color.fromARGB(255, 0, 67, 60),
-                Color.fromARGB(255, 0, 114, 103),
-              ],
-            ))),
-            Column(
-              children: [
-                if (MediaQuery.sizeOf(context).width <= 600)
-                  GestureDetector(
-                      onTap: () {
-                        if (!Scaffold.of(context).isDrawerOpen) {
-                          Scaffold.of(context).openDrawer();
-                        }
-                      },
-                      child: const Icon(Icons.menu)),
-                Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      const Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [HoverableButton()]),
-                      Row(
+      child: Stack(
+        children: [
+          Container(
+              decoration: const BoxDecoration(
+                  gradient: LinearGradient(
+            begin: Alignment.centerLeft,
+            end: Alignment.bottomRight,
+            stops: [
+              0.1,
+              0.6,
+              0.8,
+              0.9,
+            ],
+            colors: [
+              Color.fromARGB(255, 0, 0, 0),
+              Color.fromARGB(255, 0, 23, 23),
+              Color.fromARGB(255, 0, 67, 60),
+              Color.fromARGB(255, 0, 67, 60),
+            ],
+          ))),
+          Column(
+            children: [
+              if (MediaQuery.sizeOf(context).width <= 600)
+                GestureDetector(
+                    onTap: () {
+                      if (!Scaffold.of(context).isDrawerOpen) {
+                        Scaffold.of(context).openDrawer();
+                      }
+                    },
+                    child: const Icon(Icons.menu)),
+              Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [HoverableButton()]),
+                    Row(
+                      children: [
+                        Column(
+                          children: messages
+                              .map((e) => e['type'] == 'user'
+                                  ? MessageUserWidget(
+                                      label: e['message'] as String)
+                                  : MessageAIWidget(
+                                      label: e['message'] as String))
+                              .toList(),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 50,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(1000),
+                        border: Border.all(
+                          color: Colors.grey,
+                          width: 1.0,
+                        ),
+                      ),
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Column(
-                            children: messages
-                                .map((e) => e['type'] == 'user'
-                                    ? MessageUserWidget(
-                                        label: e['message'] as String)
-                                    : MessageAIWidget(
-                                        label: e['message'] as String))
-                                .toList(),
+                          Padding(
+                            padding: EdgeInsets.only(left: 16.0),
+                            child: Text(
+                              'Message...',
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 255, 255, 255)),
+                            ),
                           ),
+                          Padding(
+                            padding: EdgeInsets.only(right: 16),
+                            child: Icon(
+                              CupertinoIcons.arrow_up_square_fill,
+                              color: Colors.grey,
+                            ),
+                          )
                         ],
                       ),
-                      Container(
-                        height: 50,
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(16),
-                          border: Border.all(
-                            color: Colors.grey,
-                            width: 1.0,
-                          ),
-                        ),
-                        child: const Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(left: 8.0),
-                              child: Text(
-                                'Message...',
-                                style: TextStyle(color: Colors.grey),
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: Icon(
-                                CupertinoIcons.arrow_up_square_fill,
-                                color: Color.fromARGB(255, 77, 77, 77),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                      const SizedBox()
-                    ],
-                  ),
-                ),
-              ],
-            ),
-            Positioned(
-              bottom: 8,
-              right: 8,
-              child: Container(
-                alignment: Alignment.center,
-                width: 35,
-                height: 35,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: Colors.grey),
-                child: const Text(
-                  '?',
-                  style: TextStyle(color: Colors.white),
+                    ),
+                    const SizedBox()
+                  ],
                 ),
               ),
+            ],
+          ),
+          Positioned(
+            bottom: 8,
+            right: 8,
+            child: Container(
+              alignment: Alignment.center,
+              width: 35,
+              height: 35,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20), color: Colors.grey),
+              child: const Text(
+                '?',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
